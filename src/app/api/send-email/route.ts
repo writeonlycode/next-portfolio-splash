d0d7fde60569f35466fcc9fdbaf72637c194de3f
Iago Bozza <iagobozza@writeonlycode.io>
2023-09-27 11:16:55 +0000

    Update send-email route logic

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -5,10 +5,20 @@ export async function POST(request: NextRequest) {
   const { name, email, message } = await request.json();
   const resend = new Resend(process.env.RESEND_API_KEY);
 
+  if ( !process.env.RESEND_API_KEY ) {
+    console.error("Error: ", { message: "Resend API Key can't be empty." });
+    return NextResponse.json({ message: "Resend API Key can't be empty." }, { status: 500 });
+  }
+
+  if ( !process.env.DESTINATION_EMAIL ) {
+    console.error("Error: ", { message: "Destination email can't be empty." });
+    return NextResponse.json({ message: "Destination email can't be empty." }, { status: 500 });
+  }
+
   try {
     const data = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "next-portfolio-splash@writeonlycode.io",
+      to: process.env.DESTINATION_EMAIL,
       subject: `Contact from ${name} thought Your Next Portfolio Splash`,
       text: `${name} has sent you a message:\n\n${message}\n\nReply to them directly to their email: ${email}.`,
     });
